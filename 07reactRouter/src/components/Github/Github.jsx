9e0c1f14554c46c7d3e0ad78f3b2aee6ef17f1c3
Vansh Jakhar <vanshjakhar2003@gmail.com>
2025-05-30 19:15:22 +0530

    Context Api

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
 function Github() {
-    const data =useLoaderData();
-    // const [data,setData]=useState({});
-    // useEffect(()=>{
+  const data =useLoaderData();
+  // const [data,setData]=useState({});
+  // useEffect(()=>{
     //     fetch('https://api.github.com/users/vanshjakhar2111981208')
     //     .then(res=>res.json())
     //     .then(data=>{
-    //         console.log(data);
-    //         setData(data);
-    //     })
-    // },[])
+      //         console.log(data);
+      //         setData(data);
+      //     })
+      // },[])
   return (
     <div className='text-center bg-gray-600 p-4 text-3xl'>Github Followers : {data.followers}
     <img src={data.avatar_url}/>
